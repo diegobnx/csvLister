f3a88edaf0003cf5aecc6e1f190dea6a53692a8f
diegobnx <diego.dmnetwork@gmail.com>
2023-03-17 18:27:37 -0300

    Adicionado algumas melhorias

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from flask import Flask, render_template, request, make_response, session
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, session
 import io
+import csv
+
 
 app = Flask(__name__)
+app.secret_key = 'chave_secreta'
 
 @app.route('/')
 def home():
@@ -12,12 +16,10 @@ def home():
     clientes.insert(1, 'TODOS')  # Adiciona uma opção para todos os clientes
     return render_template('upload.html', clientes=clientes)
 
-
 @app.route('/upload', methods=['POST'])
 def upload():
     # Obtém o cliente selecionado
     cliente = request.form['cliente']
-
     # Verifica se foi selecionado um cliente
     if not cliente:
         return 'Selecione um cliente'
@@ -31,7 +33,7 @@ def upload():
         # Cria um dataframe com os dados do CSV
         df = pd.read_csv(io.StringIO(csv_data))
     except pd.errors.EmptyDataError:
-        return 'Arquivo vazio ou sem colunas para processar'
+        return 'ERRO-203: Arquivo vazio ou sem colunas para processar'
     # Filtra os dados pelo cliente selecionado na coluna "serviço"
     if cliente == 'TODOS':
         filtered_df = df
@@ -52,13 +54,32 @@ def upload():
             lambda row: row.astype(str).str.contains(r'\bPing the remote host').any(), axis=1)]
     # Ordena o dataframe pelo valor da coluna "Serviço" em ordem alfabética crescente
     filtered_df = filtered_df.sort_values(by=['Serviço'])
-
     # Converte o dataframe filtrado para uma lista de listas
     filtered_results = [filtered_df.columns.tolist()] + \
         filtered_df.values.tolist()
-
+    # Armazena a lista de resultados filtrados na sessão
+    session['filtered_results'] = filtered_results
+    session.modified = True
     # Passa a lista de dados e o cliente selecionado para o template
     return render_template('results.html', results=filtered_results, cliente=cliente)
+
+
+@app.route('/download')
+def download():
+    # Obtém a lista de resultados filtrados da sessão
+    filtered_results = session.get('filtered_results')
+    if not filtered_results:
+        return 'Lista de resultados filtrados não encontrada na sessão'
+    # Converte a lista de listas em um objeto StringIO
+    csv_buffer = io.StringIO()
+    writer = csv.writer(csv_buffer)
+    for row in filtered_results:
+        writer.writerow(row)
+    # Cria a resposta HTTP para download do arquivo
+    response = make_response(csv_buffer.getvalue())
+    response.headers['Content-Disposition'] = f'attachment; filename="results.csv"'
+    response.headers['Content-Type'] = 'text/csv'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
